505c9b83c2067bea8bd3f0ebc82f4fa4a5781f55
Keys HD <37001732+KeysHD@users.noreply.github.com>
2022-01-06 07:37:52 -0300

    ✅ Added: Water Icons

---
+++ packages/tool/src/icons/DropletFill.tsx
@@ -0,0 +1,15 @@
+function DropletFillIcon() {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="50"
+			height="50"
+			fill="currentColor"
+			viewBox="0 0 50 50"
+		>
+			<path d="M25 50a18.75 18.75 0 0018.75-18.75c0-5.172-3.506-9.075-7.6-13.631C32.044 13.05 27.344 7.822 25 0 25 0 6.25 17.769 6.25 31.25A18.75 18.75 0 0025 50zm-4.231-35.481l2.212 2.212c-.906.906-3.525 4.097-5.96 8.969l-2.793-1.4c2.563-5.128 5.366-8.603 6.54-9.781z"></path>
+		</svg>
+	);
+}
+
+export default DropletFillIcon;
