03a830e60a1d8263b11d1ec9e094901101e42779
Keys HD <37001732+KeysHD@users.noreply.github.com>
2022-01-08 09:03:43 -0300

    ✅ Added: List page

--- packages/tool/src/components/layout/List.tsx
+++ packages/tool/src/components/layout/List.tsx
@@ -1,5 +1,18 @@
+import { useRodizio } from "../../hooks/useRodizio";
+import ToolDate from "../tool/ToolDate";
+
 function ToolListPage() {
-	return <div>List page</div>;
+	const { futureEvents } = useRodizio();
+
+	return (
+		<ul className="flex flex-col">
+			{futureEvents.map((v, i) => {
+				return (
+					<ToolDate key={i} eventDate={new Date(v.data)} eventName={v.name} />
+				);
+			})}
+		</ul>
+	);
 }
 
 export default ToolListPage;
