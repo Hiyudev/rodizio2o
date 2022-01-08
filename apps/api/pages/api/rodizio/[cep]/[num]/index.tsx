f0656abe1dd37acaedfe69a931c9322ac394f0c3
Keys HD <37001732+KeysHD@users.noreply.github.com>
2022-01-08 11:17:56 -0300

    📝 Chore: Put fetch api inside of `try...catch`

--- apps/api/pages/api/rodizio/[cep]/[num]/index.tsx
+++ apps/api/pages/api/rodizio/[cep]/[num]/index.tsx
@@ -16,14 +16,13 @@ export default async function staticRodiziohandler(req, res) {
 	const num = req.query.num;
 	let address = req.query.address;
 
-	let apires;
-	if (!address) {
-		apires = await getRodizioByCep(cep, num);
-	} else {
-		apires = await getRodizio(address);
-	}
-
 	try {
+		let apires;
+		if (!address) {
+			apires = await getRodizioByCep(cep, num);
+		} else {
+			apires = await getRodizio(address);
+		}
 		res.status(200).json(apires);
 	} catch (err) {
 		res.status(400).json({ error: err.message });
