3e0fb11edb702ec36464365d3a190ac56ef54a87
Keys HD <37001732+KeysHD@users.noreply.github.com>
2022-01-10 20:39:01 -0300

    📝 Changed: Made APP not centralized anymore

--- packages/tool/src/components/layout/App.tsx
+++ packages/tool/src/components/layout/App.tsx
@@ -1,38 +1,18 @@
-import { useState } from "react";
 import { RodizioWrapper } from "../../hooks/useRodizio";
-import { RodizioPages } from "../../shared";
 import ToolContainer from "../tool/ToolContainer";
-import ToolConfigPage from "./Config";
-import ToolHomePage from "./Home";
-import ToolListPage from "./List";
-import Navbar from "./Nav";
 
-export const App = ({ hasThemeSwitcher, className }) => {
-	const [page, setPage] = useState(RodizioPages.Home);
+interface IApp {
+	hasThemeSwitcher: boolean;
+	className: string;
+}
 
-	const Container = () => {
-		switch (page) {
-			case RodizioPages.Home:
-				return <ToolHomePage />;
-			case RodizioPages.Config:
-				return <ToolConfigPage />;
-			case RodizioPages.List:
-				return <ToolListPage />;
-		}
-	};
-
+export const App = ({ hasThemeSwitcher, className }: IApp) => {
 	return (
 		<RodizioWrapper>
-			<ToolContainer className={className}>
-				<Navbar
-					hasThemeSwitcher={hasThemeSwitcher}
-					currentPage={page}
-					changePage={setPage}
-				/>
-				<main className="p-4">
-					<Container />
-				</main>
-			</ToolContainer>
+			<ToolContainer
+				hasThemeSwitcher={hasThemeSwitcher}
+				className={className}
+			/>
 		</RodizioWrapper>
 	);
 };
