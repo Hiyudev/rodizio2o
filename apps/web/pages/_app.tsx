db3499ef6bfe035afeae0a8447bfc70a34ab235c
Keys HD <37001732+KeysHD@users.noreply.github.com>
2022-01-13 09:11:58 -0300

    ✅ Added: Website title

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,9 +1,14 @@
 import "../styles/global.css";
 import { ThemeProvider } from "next-themes";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<ThemeProvider enableSystem attribute="class">
+			<Head>
+				<title>Rodizio2O</title>
+			</Head>
+
 			<Component {...pageProps} />
 		</ThemeProvider>
 	);
