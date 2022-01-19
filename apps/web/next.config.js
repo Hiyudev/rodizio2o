8ee43165b7de906ebc5ef0c0ba5dcef623c50cee
Keys HD <37001732+KeysHD@users.noreply.github.com>
2022-01-19 08:00:38 -0300

    📝 Fixed: PWA

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,12 +1,16 @@
 const withTM = require("next-transpile-modules")(["tool"]);
 const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withTM(
 	withPWA({
 		reactStrictMode: true,
 		pwa: {
 			dest: "public",
-			scope: "/app",
+			register: true,
+			skipWaiting: true,
+			runtimeCaching,
+			buildExcludes: [/middleware-manifest.json$/],
 		},
 	})
 );
