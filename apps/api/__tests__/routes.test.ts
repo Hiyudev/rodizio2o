defcdebfe87157e3ac4786c58adc2189c06f916e
Keys HD <37001732+KeysHD@users.noreply.github.com>
2022-01-19 11:55:58 -0300

    📝 Chore: More accurate tests

--- apps/api/__tests__/routes.test.ts
+++ apps/api/__tests__/routes.test.ts
@@ -13,35 +13,25 @@ const testRodizioResponse = (res) => {
 		const start = new Date(data.current.INICIO);
 		const end = new Date(data.current.RETOMADA);
 		expect(now).toBeBetween(start, end);
+		expect(data.next[0].INICIO).not.toEqual(start.getTime())
+		expect(data.next[0].RETOMADA).not.toEqual(end.getTime())
 	} else {
 		expect(data.current).toBeNull();
 	}
 
+
 	expect(data.next).toHaveLength(4);
 
-	data.next.map(v => {
+	data.next.forEach(v => {
 		expect(v.INICIO).toBeAfter(now);
 	})
 
 	expect(data.observation).toBeDefined();
-	expect(data.location).toMatch(/Alameda Presidente Taunay 1720/)
+	expect(data.location).toBeDefined();
+	expect(data.location.length).toBeGreaterThanOrEqual(5);
 }
 
-describe("Test API Routes", () => {
-	it("is API Route [?cep=...?num=...] for rodizio data working", async () => {
-		const { req, res } = createMocks({
-			method: 'GET',
-			query: {
-				cep: "80430-042",
-				num: "1720"
-			},
-		});
-
-		await rodizioHandler(req, res);
-
-		testRodizioResponse(res);
-	})
-
+describe('Test API Suggestion routes', () => {
 	it("is API Route [?address=...] for rodizio data working", async () => {
 		const { req, res } = createMocks({
 			method: 'GET',
@@ -51,20 +41,6 @@ describe("Test API Routes", () => {
 		});
 
 		await rodizioHandler(req, res);
-
-		testRodizioResponse(res);
-	})
-
-	it("is API Route [/[cep]/[num]] for rodizio data working", async () => {
-		const { req, res } = createMocks({
-			method: 'GET',
-			query: {
-				cep: "80430-042",
-				num: "1720"
-			},
-		});
-
-		await staticRodiziohandler(req, res);
 
 		testRodizioResponse(res);
 	})
@@ -84,5 +60,38 @@ describe("Test API Routes", () => {
 
 		expect(data.length).toBe(1);
 		expect(data[0]).toMatch(/Alameda Presidente Taunay 1720/)
+	})
+
+})
+
+describe.each([
+	"80040-330", "82100-080", "82560-260", "80220-150"
+])("Test API Routes for %i", (cep) => {
+	it("is API Route [?cep=...?num=...] for rodizio data working", async () => {
+		const { req, res } = createMocks({
+			method: 'GET',
+			query: {
+				cep: cep,
+				num: "1720"
+			},
+		});
+
+		await rodizioHandler(req, res);
+
+		testRodizioResponse(res);
+	})
+
+	it("is API Route [/[cep]/[num]] for rodizio data working", async () => {
+		const { req, res } = createMocks({
+			method: 'GET',
+			query: {
+				cep: cep,
+				num: "1720"
+			},
+		});
+
+		await staticRodiziohandler(req, res);
+
+		testRodizioResponse(res);
 	})
 })
